4b823382ec34bddffe1f81ad4fd0e422d728da4a
Vadym Martsynovskyy <maldorlevr@gmail.com>
2015-05-17 04:06:22 +0200

    Made week view cache, created ability to refresh cache, and switch between weeks

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,6 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-
-
 Date.prototype.rotation = null;
-Date.prototype.type = null;
+Date.prototype.day_type = null;
+Date.prototype.today = null;
 Date.prototype.announcement = null;
 Date.prototype.school_start_time = null;
 Date.prototype.school_end_time = null;
@@ -18,7 +12,7 @@ Date.prototype.getShortDay = function() {
   return days[this.getDay()];
 }
 Date.prototype.getShortMonth = function() {
-  var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEPT", "OCT", "NOV", "DEC"];;
+  var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JLY", "AUG", "SEP", "OCT", "NOV", "DEC"];;
   return months[this.getMonth()];
 }
 Date.prototype.getTwoDigitDate = function() {
@@ -43,7 +37,6 @@ Date.prototype.getNumberOfWeekdaysSince = function(startDate) {
   }
   return weekdayCounter;
 };
-
 Date.prototype.getRotation = function() {
   if(this.getDay() !== 6 && this.getDay() !== 0) {
     // Before deploying add more than one year's worth of start dates!
@@ -70,15 +63,25 @@ Date.prototype.getRotation = function() {
             return [1, 2, 3, 4];
             break;
         case 7:
-            return [1, 2, 3, 4];
+            return [5, 6, 7, 8];
             break;
         case 8:
-            return [1, 2, 3, 4];
+            return [2, 3, 1, 4];
             break;
         case 9:
-            return [1, 2, 3, 4];
+            return [6, 7, 5, 8];
             break;
     }
+  }
+}
+
+Date.prototype.getStartOfWeek = function() {
+  var i = this;
+  while(true) {
+    if (i.getDay() === 0) {
+      return i;
+    }
+    i.setDate(i.getDate() - 1);
   }
 }
 
@@ -99,57 +102,103 @@ function twentyFourHourToAmPm(timestring) {
 
 var xenon = angular.module('xenon', ['ionic', 'ngResource']);
 
-xenon.config(function($stateProvider, $urlRouterProvider) {
+xenon.config(function($stateProvider, $urlRouterProvider, $anchorScrollProvider) {
   $urlRouterProvider.otherwise("/");
 
-  $stateProvider.state('plan', {
-    url: "/plan",
+  $anchorScrollProvider.disableAutoScrolling()
+
+  $stateProvider.state('week', {
+    url: "/week?date",
     views: {
-      week: {
+      'week': {
         templateUrl: "templates/week.html",
         controller: 'WeekCtrl'
       },
-      day: {
-        templateUrl: "templates/day.html"
+    }
+  });
+
+  $stateProvider.state('day', {
+    url: '/day',
+    views: {
+      'day': {
+        templateUrl: "templates/day.html",
+        controller: 'DayCtrl'
       }
     }
   });
 });
 
-xenon.factory('Day', function($resource) {
+xenon.factory('Day', function($resource, $cacheFactory) {
+  days_cache = $cacheFactory('days');
   return $resource('http://107.170.252.240/days/', {}, {
-    getDay: {method: 'GET', url:'http://107.170.252.240/days/?d=:date&m=:month&y=:year', params: {date:'@date', month: '@month', year: '@year'}},
+    getDay: {cache: days_cache, method: 'GET', url:'http://107.170.252.240/days/?d=:date&m=:month&y=:year', params: {date:'@date', month: '@month', year: '@year'}},
   });
 });
 
-xenon.controller('WeekCtrl',['$scope', '$http', 'Day',
-  function ($scope, $http, Day) {
-    var day = Date.now();
-    $scope.days = [];
-    for (var i = 0; i < 7; i++) {
-      day_obj = new Date(day);
-      day_obj.setHours(0,0,0,0);
-      rotation = day_obj.getRotation();
-      if (rotation) {
-        day_obj.rotation = rotation[0] + ' ' + rotation[1] + ' ' + rotation[2] + ' ' + rotation[3];
-      }
-      var web_day = Day.getDay({date: day_obj.getDate(), month: day_obj.getMonth() + 1, year: day_obj.getFullYear()}, function (result) {
-        if (result.count > 0) {
-          for (var iter = 0; iter < 7; iter++) {
-            if(new Date(result.results[0].date + " PDT").getDate() === $scope.days[iter].getDate()) {
-              $scope.days[iter].name = result.results[0].name;
-              $scope.days[iter].type = result.results[0].type;
-              $scope.days[iter].announcement = result.results[0].announcement;
-              if (result.results[0].school_start_time && result.results[0].school_end_time) {
-                $scope.days[iter].diplaySchoolTime = twentyFourHourToAmPm(result.results[0].school_start_time) + " - " + twentyFourHourToAmPm(result.results[0].school_end_time);
+xenon.controller('DayCtrl', ['$scope', 'Day', function ($scope, Day) {
+
+}]);
+
+xenon.controller('WeekCtrl',['$scope', '$location', '$ionicViewSwitcher', '$cacheFactory', 'Day',
+  function ($scope, $location, $ionicViewSwitcher, $cacheFactory, Day) {
+    function renderWeekFromDate(date_arg) {
+      $scope.days = [];
+      var days = [];
+      date_arg.setHours(0,0,0,0);
+      var next_date = date_arg;
+      for (var i = 0; i < 7; i++) {
+        var date = new Date(next_date);
+        rotation = date.getRotation();
+        if (date.valueOf() === new Date(Date.now()).setHours(0,0,0,0)) {
+          date.today = true;
+        }
+        if (rotation) {
+          date.rotation = rotation[0] + ' ' + rotation[1] + ' ' + rotation[2] + ' ' + rotation[3];
+        }
+        var web_day = Day.getDay({date: date.getDate(), month: date.getMonth() + 1, year: date.getFullYear()}, function (result) {
+          if (result.count > 0) {
+            for (var iter = 0; iter < 7; iter++) {
+              if(new Date(result.results[0].date + " PDT").getDate() === $scope.days[iter].getDate()) {
+                $scope.days[iter].name = result.results[0].name;
+                $scope.days[iter].day_type = result.results[0].day_type;
+                $scope.days[iter].announcement = result.results[0].announcement;
+                if (result.results[0].school_start_time && result.results[0].school_end_time) {
+                  $scope.days[iter].diplaySchoolTime = twentyFourHourToAmPm(result.results[0].school_start_time) + " - " + twentyFourHourToAmPm(result.results[0].school_end_time);
+                }
               }
             }
           }
-        }
-      });
-      $scope.days.push(day_obj);
-      day += (1000 * 60 * 60 * 24);
+        });
+        $scope.days.push(date);
+        next_date.setDate(date.getDate() + 1);  
+      }   
     }
+
+    $scope.doRefresh = function() {
+      $cacheFactory.get('days').removeAll();
+      $scope.$broadcast('scroll.refreshComplete');
+    }
+
+    $scope.incrementWeek = function() {
+      if (!$location.search().date) {
+        $location.search('date', Date.now());
+      }
+      var dateToRender = new Date($location.search().date).getStartOfWeek();
+      dateToRender.setDate(dateToRender.getDate() + 7);
+      $location.search('date', dateToRender.valueOf());
+    }
+    $scope.decrementWeek = function() {
+      if (!$location.search().date) {
+        $location.search('date', Date.now());
+      }
+      var dateToRender = new Date($location.search().date).getStartOfWeek();
+      dateToRender.setDate(dateToRender.getDate() - 7);
+      $location.search('date', dateToRender.valueOf());
+    }
+    if (!$location.search().date) {
+      $location.search('date', Date.now());
+    }
+    renderWeekFromDate(new Date($location.search().date).getStartOfWeek());
 }]);
 
 
